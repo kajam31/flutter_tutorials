4917bde9f4c9403c13f335c50f09384d40c7fa1e
kajam31 <Jaspervandemoortel@outlook.be>
2021-11-13 14:30:11 +0100

    functions

--- dart_test/variables.dart
+++ dart_test/variables.dart
@@ -1,4 +1,18 @@
 // ######## variables
 // dart is a static variable type language => when a variable is assigned a specific dartatype it can't be cahnged to another data type
+// if you use the dynamic variale type you can use it like in python
 
-test
+void main() {
+  int age = 30;
+  String name = "jasper";
+  bool is_night = false;
+  dynamic variable = "my name";
+  variable = 5;
+  
+  print(age);
+  print(name);
+  print(is_night);
+  print(variable);
+}
+
+
