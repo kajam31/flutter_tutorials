1d6ae4f5687a83b6c1685bba42c23d8bf410dafa
kajam31 <Jaspervandemoortel@outlook.be>
2021-11-13 14:19:06 +0100

    hello world

--- dart_test/hello_world.dart
+++ dart_test/hello_world.dart
@@ -1,5 +1,12 @@
+/* 
 void main() {
     for (int i = 0; i< 5; i++) {
         print("hello ${i+1}");
     }
 }
+ */
+
+void main(){
+  print("hello world");
+}
+
---
+++ dart_test/variables.dart
@@ -0,0 +1,3 @@
+// ######## variables
+// dart is a static variable type language => when a variable is assigned a specific dartatype it can't be cahnged to another data type
+
