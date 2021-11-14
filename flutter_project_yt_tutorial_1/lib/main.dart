ff73005b61fcce2e5b32a36f5db7259d659c9517
kajam31 <Jaspervandemoortel@outlook.be>
2021-11-14 22:26:35 +0100

    added custom fonts, will now update evertime saved

--- flutter_project_yt_tutorial_1/lib/main.dart
+++ flutter_project_yt_tutorial_1/lib/main.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-    home: Scaffold(
-        appBar: AppBar(
-          title: Text("my test app"),
-          centerTitle: true,
-          backgroundColor: Colors.green[800],
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  // ignore: dead_code
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("my test app"),
+        centerTitle: true,
+        backgroundColor: Colors.green[800],
+      ),
+      body: Center(
+        child: Text(
+          'this is a test!',
+          style: TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.purple[800],
+            fontFamily: 'Estonia',
+          ),
         ),
-        body: Center(
-          	child: Text(
-            	'this a test!',
-            	style: TextStyle(
-             		fontSize: 20.0,
-              		fontWeight: FontWeight.bold,
-              		letterSpacing: 2.0,
-              		color: Colors.purple[800],
-                  fontFamily: "Shalimar",
-            	),
-          	),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Text("click"),
-          backgroundColor: Colors.red[600],
-        ),
-    ),
-));
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Text("click"),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
