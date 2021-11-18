084e9f607f4fe459c3a1c38c6071105e87db0c2c
kajam31 <Jaspervandemoortel@outlook.be>
2021-11-18 23:00:26 +0100

    made class

--- lists_of_darta/lib/quote.dart
+++ lists_of_darta/lib/quote.dart
@@ -1,4 +1,18 @@
 class Quote {
   late String text;
-  
+  late String author;
+
+  Quote({required this.text, required this.author});
 }
+// when  not using the {} you have to input the data in the order that the parameters are written
+// which can lead to confusion and other problems
+// Quote myquote = Quote("this is the quote text", "autor name");
+// Quote(String text, String author) {
+//   this.text = text;
+//   this.author = author;
+// }
+
+// when usin the {} around the parameter input of the function it makes the parameters named parameters
+// this makes it so that you have to devine what you are declaring when You make a new class element
+
+// Quote myquote = Quote(text: "this is the quote text", author: "autor name");
