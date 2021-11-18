111ca321aeaf527b3850e237d9646276490b135c
kajam31 <Jaspervandemoortel@outlook.be>
2021-11-18 23:39:12 +0100

    added font, added cards and personal cardish template

--- lists_of_darta/lib/main.dart
+++ lists_of_darta/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "quote.dart";
 
 void main() => runApp(MaterialApp(
       home: QuoteList(),
@@ -12,11 +13,82 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-    "Every second counts, spend them wisely",
-    "The truth is rarely pure and never simple",
-    "Take 6 months for yourself to change the rest of yourself"
+  List<Quote> quotes = [
+    Quote(author: "me", text: "Every second counts, spend them wisely"),
+    Quote(
+        author: "Oscar Wilde",
+        text: "The truth is rarely pure and never simple"),
+    Quote(
+        author: "Elon Musk",
+        text: "Take 6 months for yourself to change the rest of yourself"),
   ];
+
+  List<Quote> cardQuotes = [
+    Quote(author: "me", text: "Every second counts, spend them wisely"),
+    Quote(
+        author: "Oscar Wilde",
+        text: "The truth is rarely pure and never simple"),
+    Quote(
+        author: "Elon Musk",
+        text: "Take 6 months for yourself to change the rest of yourself"),
+  ];
+  Widget quoteTemplate(cardQuote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            cardQuote.text,
+            style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+          ),
+          SizedBox(height: 6.0),
+          Text(
+            cardQuote.author,
+            style: TextStyle(
+              color: Colors.grey[400],
+              fontSize: 16.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget personalTemplate(quote) {
+    return Center(
+      child: (Container(
+        padding: EdgeInsets.all(20.0),
+        color: Colors.indigoAccent[700],
+        width: 350.0,
+        margin: EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Center(
+              child: Text(
+                quote.text,
+                style: TextStyle(
+                  color: Colors.deepOrange[600],
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.0,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              " - ${quote.author}",
+              style: TextStyle(
+                color: Colors.grey[200],
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.0,
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,28 +99,15 @@ class _QuoteListState extends State<QuoteList> {
         centerTitle: true,
         backgroundColor: Colors.teal[900],
       ),
-      body: Column(
-        children: quotes.map((quote) {
-          return Center(
-            child: (Container(
-              padding: EdgeInsets.all(20.0),
-              color: Colors.indigoAccent[700],
-              width: 350.0,
-              margin: EdgeInsets.all(10.0),
-              child: Center(
-                child: Text(
-                  quote,
-                  style: TextStyle(
-                    color: Colors.deepOrange[600],
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-              ),
-            )),
-          );
-        }).toList(),
-      ),
+      body: Column(children: [
+        Column(
+          children: quotes.map((quote) => personalTemplate(quote)).toList(),
+        ),
+        Column(
+          children:
+              cardQuotes.map((cardQuote) => quoteTemplate(cardQuote)).toList(),
+        )
+      ]),
     );
   }
 }
