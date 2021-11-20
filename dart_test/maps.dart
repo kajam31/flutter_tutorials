9485cb913065608da74646e7edd897e61f475e44
kajam31 <Jaspervandemoortel@outlook.be>
2021-11-20 09:17:37 +0100

    added map file in dart test

---
+++ dart_test/maps.dart
@@ -0,0 +1,10 @@
+// maps are like dictionaries in python a set of key and value pairs
+
+void main() {
+  Map student = {
+    "name": "japer",
+    "age": 17,
+  };
+  print(student["name"]);
+  print(student['age']);
+}
--- world_time_app/lib/pages/home.dart
+++ world_time_app/lib/pages/home.dart
@@ -10,6 +10,6 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("home screen"));
+    return Scaffold(body: SafeArea(child: Text("home screen")));
   }
 }
