343596b0a716685c07d8d850f5f5e09acd80569c
kajam31 <Jaspervandemoortel@outlook.be>
2021-11-20 21:12:57 +0100

    added backgroudn images that change depending on the time ( the range when day and night is declared in world_time.dart) (33)

--- world_time_app/lib/pages/home.dart
+++ world_time_app/lib/pages/home.dart
@@ -19,42 +19,55 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-
     data = ModalRoute.of(context)!.settings.arguments as Map;
     print(data);
+
+    // set background
+    String bgImage = data["isDaytime"] ? "day.png" : "night.png";
+
     return Scaffold(
-        body: SafeArea(
-            child: Padding(
-      padding: const EdgeInsets.fromLTRB(0.0, 120.0, 0.0, 0.0),
-      child: Column(
-        children: <Widget>[
-          TextButton.icon(
-            onPressed: () {
-              Navigator.pushNamed(context, "/location");
-            },
-            icon: Icon(Icons.edit_location),
-            label: Text("edit location"),
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/$bgImage"),
+              fit: BoxFit.cover,
+            ),
           ),
-          SizedBox(height: 20.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                data["location"],
-                style: TextStyle(
-                  fontSize: 28.0,
-                  letterSpacing: 2.0,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 120.0, 0.0, 0.0),
+            child: Column(
+              children: <Widget>[
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/location");
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text("edit location"),
                 ),
-              ),
-            ],
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data["location"],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  data["time"],
+                  style: TextStyle(fontSize: 66.0),
+                )
+              ],
+            ),
           ),
-          SizedBox(height: 20.0),
-          Text(
-            data["time"],
-            style: TextStyle(fontSize: 66.0),
-          )
-        ],
+        ),
       ),
-    )));
+    );
   }
 }
